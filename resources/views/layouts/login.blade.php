e37b5c0b4c16112759fd35ddf4c00fb3982cb18c
herneyruiz36@gmail.com <herneyruiz36@gmail.com>
2020-02-04 16:19:23 -0500

    agregando el layout de login

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ecommerce - @yield('title_page')</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/css/connect.css') }}">
+
+    <script src="https://kit.fontawesome.com/7ef6ca5d1d.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    @section('content')
+    @show
+    
+</body>
+
+</html>

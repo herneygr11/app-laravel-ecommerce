0b9a96a40fa268c7415916df53924f324a799585
herneyruiz36@gmail.com <herneyruiz36@gmail.com>
2020-02-05 17:12:38 -0500

    cambiando de email a password el input

--- resources/views/connect/login.blade.php
+++ resources/views/connect/login.blade.php
@@ -5,51 +5,66 @@
 @section('content')
 
 <div class="box box_login">
+    
+    <div class="header">
+        <a href="{{ route('index') }}">
+            <img src="{{ asset('src/img/logo.png') }}" alt="logo">
+        </a>
+    </div>
+    
+    <div class="inside">
 
-    {!! Form::open( [ 'route' => 'login'] ) !!}
+        {!! Form::open( [ 'route' => 'login'] ) !!}
 
-    <div class="form-group">
+        <div class="form-group">
 
-        {!! Form::label('email', 'Correo eletrónico :') !!}
+            {!! Form::label('email', 'Correo eletrónico :') !!}
 
-        <div class="input-group">
-            
-            <div class="input-group-prepend">
-                
-                <div class="input-group-text">
-                    <i class="far fa-envelope-open"></i>
+            <div class="input-group">
+
+                <div class="input-group-prepend">
+
+                    <div class="input-group-text">
+                        <i class="far fa-envelope-open"></i>
+                    </div>
+
                 </div>
-                
+
+                {!! Form::email( 'email', null, [ 'class' => 'form-control' ] ) !!}
+
             </div>
-            
-            {!! Form::email( 'email', null, [ 'class' => 'form-control' ] ) !!}
-            
+
+        </div>
+
+        <div class="form-group">
+
+            <label for="password">Contraseña:</label>
+            <div class="input-group">
+
+                <div class="input-group-prepend">
+
+                    <div class="input-group-text">
+                        <i class="fas fa-lock"></i>
+                    </div>
+
+                </div>
+
+                {!! Form::password( 'password', [ 'class' => 'form-control' ] ) !!}
+
+            </div>
+
+        </div>
+
+        {!! Form::submit('Ingresar', [ 'class' => 'btn btn-success w-100' ]) !!}
+
+        {!! Form::close() !!}
+
+        <div class="footer mt-3">
+            <a href="{{ route('register') }}">¿No tienes una cuenta?, Registrate</a>
+            <a href="#">Recuperar contraseña</a>
         </div>
         
     </div>
-
-    <div class="form-group">
-
-        <label for="password">Contraseña:</label>
-        <div class="input-group">
-            
-            <div class="input-group-prepend">
-                
-                <div class="input-group-text">
-                    <i class="fas fa-lock"></i>
-                </div>
-                
-            </div>
-            
-            {!! Form::email( 'password', null, [ 'class' => 'form-control' ] ) !!}
-            
-        </div>
-
-    </div>
-
-    {!! Form::submit('Ingresar', [ 'class' => 'btn btn-success w-100' ]) !!}
-
-    {!! Form::close() !!}
 
 </div>
 

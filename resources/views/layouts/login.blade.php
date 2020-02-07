d2a0c4f431b0da86c31fd2b0325092ebc2b63db3
herneyruiz36@gmail.com <herneyruiz36@gmail.com>
2020-02-07 16:03:54 -0500

    agregando jquery

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -12,6 +12,7 @@
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/connect.css') }}">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/7ef6ca5d1d.js" crossorigin="anonymous"></script>
 
 </head>

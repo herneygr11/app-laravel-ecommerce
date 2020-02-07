160891f47cd1ade5d12f76a08cd66f52b0735909
herneyruiz36@gmail.com <herneyruiz36@gmail.com>
2020-02-07 16:04:16 -0500

    identanto la migracion de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->integer('role')->default(0);
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
             $table->timestamps();
         });

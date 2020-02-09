6805b4b669a3b1e2e9e693b92980fd1bfb173a63
RuizHerney <herneyruiz36@gmail.com>
2020-02-09 15:10:49 -0500

    creando los metodos de login y logout

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use phpDocumentor\Reflection\Types\Boolean;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 
 class ConnectController extends Controller
 {
+/**
+     * Retorna la vista principal de login
+     * 
+     *  @author Herney Ruiz-Meza
+     * 
+     *  @param not
+     *  @return view login 
+     */
+    public function __construct() 
+    {
+        $this->middleware('guest')->except('getLogout');
+    } # End method __construct
 
     /**
      * Retorna la vista principal de login
@@ -27,6 +41,27 @@ class ConnectController extends Controller
     } # End method getLogin
 
     /**
+     * Valida las credenciales del usuario
+     * 
+     *  @author Herney Ruiz-Meza
+     * 
+     *  @param not
+     *  @return View 
+     */
+    public function postLogin(LoginRequest $request)
+    {
+        // Verificamos las credenciales, y quitanos el token de el array request
+        if ( Auth::attempt($request->except('_token'), true) ){
+            
+            return redirect()->route('index');
+
+        }else{
+            
+            return back()->with('message_login', 'Correo o/y Contraseña incorrecta.');
+        }
+    } # End method postLogin
+
+    /**
      * Retorna la vista principal de registro
      * 
      *  @author Herney Ruiz-Meza
@@ -40,12 +75,12 @@ class ConnectController extends Controller
     } # End method getRegister
 
     /**
-     * Envia los datos de regsitro al modelo Connect
+     * Envia los datos de regsitro al modelo User
      * 
      *  @author Herney Ruiz-Meza
      * 
      *  @param RegisterRequest $request
-     *  @return 
+     *  @return View login
      */
     public function postRegister(RegisterRequest $request)
     {
@@ -57,5 +92,20 @@ class ConnectController extends Controller
             return redirect()->route('login');
         }
     } # End method postRegister
+
+    /**
+     * Cerrar la session 
+     * 
+     *  @author Herney Ruiz-Meza
+     * 
+     *  @param not
+     *  @return View index
+     */
+    public function getLogout()
+    {
+        Auth::logout();
+
+        return redirect()->route('index');
+    } # End method getLogout
 
 } # End class ConnectController

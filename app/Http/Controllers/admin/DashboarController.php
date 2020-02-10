775c34f61cf178a64352f425a76b9f3c9e0c8979
herneyruiz36@gmail.com <herneyruiz36@gmail.com>
2020-02-10 16:11:41 -0500

    creando el controlador de admin

---
+++ app/Http/Controllers/admin/DashboarController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboarController extends Controller
+{
+    /**
+     * Llamamos librerias necesarias y middleware
+     */
+    public function __construct() {
+        $this->middleware('auth');
+        $this->middleware('isadmin');
+    } # End method __construct
+
+    /**
+     * Retorna la vista principal del dashboard
+     *
+     *  @author Herney Ruiz-Meza
+     *
+     *  @return view dashboar
+     */
+    public function getDashboar()
+    {
+        return view('admin.dashboard');
+    } # End method getDashboar
+
+} # End class DashboarController

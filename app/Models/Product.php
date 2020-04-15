54631d912c37d2ac8eb44a827da4f9733882d19e
Herney Ruiz <herneyruiz36@gmail.com>
2020-04-14 21:44:05 -0500

    model de producto

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Product extends Model
+{
+    use Sluggable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+                'unique' => true,
+            ]
+        ];
+    }
+
+} # Enc clas Product

d93ab01a3e9a785e623cf00a06a198f2637efdcf
Herney Ruiz <herneyruiz36@gmail.com>
2020-05-11 18:32:25 -0500

    actualizacion de laravel 6 a 7

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -29,27 +29,37 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable   $exception
      * @return void
      *
-     * @throws \Exception
+     * @throws \Throwable 
      */
-    public function report(Exception $exception)
+    public function report(Throwable  $exception)
     {
         parent::report($exception);
+    }
+
+    public function shouldReport(Throwable $exception)
+    {
+        parent::shouldReport($exception);
     }
 
     /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Symfony\Component\HttpFoundation\Response
      *
-     * @throws \Exception
+     * @throws \Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    public function renderForConsole($output, Throwable $exception)
+    {
+        parent::renderForConsole($output, $exception);
     }
 }

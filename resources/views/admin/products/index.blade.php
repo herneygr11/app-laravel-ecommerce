4b0032a2be0591021155b74f43a57d661d53e829
Herney Ruiz <herneyruiz36@gmail.com>
2020-05-13 08:20:47 -0500

    listando productos

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -3,12 +3,12 @@
 @section('title_page', 'productos')
 
 @section('breadcrumb')
-    <li class="breadcrumb-item">
-        <a href="{{ route('products.index') }}">
-            <i class="fas fa-boxes"></i>
-            Productos
-        </a>
-    </li>
+<li class="breadcrumb-item">
+    <a href="{{ route('products.index') }}">
+        <i class="fas fa-boxes"></i>
+        Productos
+    </a>
+</li>
 @endsection
 
 @section('content')
@@ -35,8 +35,54 @@
 
         <div class="inside">
 
-            <table class="table">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Categoria</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                    <tr class="">
+                        <td> {{ $product->id }} </td>
+                        <td>
+                            <a data-fancybox="gallery" href="{{ asset($product->image_path . '/m_' . $product->image) }}">
+                                <img src="{{ asset($product->image_path . '/m_' . $product->image) }}" width="64"
+                                    alt="{{ $product->name }}">
+                            </a>
+                        </td>
+                        <td> {{ $product->name }} </td>
+                        <td> {{ $product->category->name }} </td>
+                        <td> {{ $product->price }} </td>
+                        <td class="d-flex">
 
+                            <a href="{{ route('products.edit', ['slug' =>  $product->id]) }}"
+                                class="btn btn-primary mx-1 btn-accion">
+
+                                <i class="fas fa-edit"></i>
+
+                            </a>
+
+                            {!! Form::open( [ 'route' => ['products.delete', 'id' => $product->id, 'method' => 'delete']
+                            ] ) !!}
+
+                            <button class="btn btn-danger mx-1 btn-accion">
+
+                                <i class="fas fa-trash-alt"></i>
+
+                            </button>
+
+                            {!! Form::close() !!}
+
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
             </table>
 
         </div>

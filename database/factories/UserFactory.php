5f45f85d9e0a346655b9bc2b26005a8ca4234e13
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-06 18:44:21 -0500

    creando factory de user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,18 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+
+    $name = $faker->name;
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'role'              => $faker->numberBetween(0,1),
+        'status'            => $faker->randomElement([0,1,100]),
+        'name'              => $name,
+        'last_name'         => $faker->name,
+        'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        'slug'              => Str::slug($name),
+        'remember_token'    => Str::random(10),
     ];
 });

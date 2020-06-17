944bf0be76ea66c8f7f123628f89c731e474988a
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-16 22:15:51 -0500

    verificando permisos

--- resources/views/includes/dashboard/sidebar.blade.php
+++ resources/views/includes/dashboard/sidebar.blade.php
@@ -20,30 +20,38 @@
 
     <div class="main">
         <ul>
+            @if (getUserPermission(Auth::user()->permissions, "admin.index") === "true")
             <li>
                 <a href="{{ route('admin.index') }}" class="lk-dashboard">
                     <i class="fas fa-home"></i>
                     Dashboard
                 </a>
             </li>
+            @endif
+            @if (getUserPermission(Auth::user()->permissions, "users.index") === "true")
             <li>
                 <a href="{{ route('users.index') }}" class="lk-user">
                     <i class="fas fa-user"></i>
                     Usuarios
                 </a>
             </li>
+            @endif
+            @if (getUserPermission(Auth::user()->permissions, "products.index") === "true")
             <li>
                 <a href="{{ route('products.index') }}" class="lk-product">
                     <i class="fas fa-boxes"></i>
                     Productos
                 </a>
             </li>
+            @endif
+            @if (getUserPermission(Auth::user()->permissions, "categories.index") == "true")
             <li>
                 <a href="{{ route('categories.index') }}" class="lk-category">
                     <i class="fas fa-folder-open"></i>
                     Categorias
                 </a>
             </li>
+            @endif
         </ul>
     </div>
 </div>

671c2c40a7be498accf7e72893ae50dc480891b3
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-18 10:25:21 -0500

    test de creacion de categoria

--- tests/Feature/CreateCategoryTest.php
+++ tests/Feature/CreateCategoryTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Str;
 use Tests\TestCase;
 
 class CreateCategoryTest extends TestCase
@@ -16,17 +15,128 @@ class CreateCategoryTest extends TestCase
      * @return void
      * @test
      */
-    public function an_authenticated_user_can_create_categories()
+    public function quests_users_can_not_create_categories(): void
     {
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
-
-        $this->post(route("categories.save"), [
+        $response = $this->post(route("categories.save"), [
             "name" => "Camisas", "icon" => '<i class="fas fa-car"></i>'
         ]);
 
+        $response->assertStatus(302);
+        $response->assertRedirect(route("login"));
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function common_users_can_not_create_categories(): void
+    {
+        $user = factory(User::class)->create([
+            "role" => 0
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->post(route("categories.save"), [
+            "name" => "Camisas", "icon" => '<i class="fas fa-car"></i>'
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("index"));
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function banned_users_can_not_create_categories(): void
+    {
+        $user = factory(User::class)->create([
+            "status" => 100
+        ]);
+
+        $response = $this->post(route("categories.save"), [
+            "name" => "Camisas", "icon" => '<i class="fas fa-car"></i>'
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("login.index"));
+    }
+
+    /**
+     * @return User
+     * @test
+     */
+    public function a_authenticated_user_with_role_admin_and_not_banned(): User
+    {
+        $user = factory(User::class)->create([
+            "role"      => 1,
+            "status"    => 0
+        ]);
+
+        $this->actingAs($user);
+        $this->assertAuthenticatedAs($user);
+
+        return $user;
+    }
+
+    /**
+     * @return void
+     * @test
+     * @depends a_authenticated_user_with_role_admin_and_not_banned
+     */
+    public function a_category_required_name_and_icon(User $user): void
+    {
+        $this->actingAs($user);
+
+        $response = $this->from(route("categories.index"))->post(route("categories.save"), [
+            "name" => "",
+            "icon" => ""
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("categories.index"));
+        $response->assertSessionHasErrors(["name", "icon"]);
+    }
+
+    /**
+     * @return void
+     * @test
+     * @depends a_authenticated_user_with_role_admin_and_not_banned
+     */
+    public function a_category_required_name_alpha(User $user): void
+    {
+        $this->actingAs($user);
+
+        $response = $this->from(route("categories.index"))->post(route("categories.save"), [
+            "name" => "hola hola",
+            "icon" => '<i class="fas fa-car"></i>'
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("categories.index"));
+        $response->assertSessionHasErrors(["name"]);
+    }
+
+    /**
+     * @return void
+     * @test
+     * @depends a_authenticated_user_with_role_admin_and_not_banned
+     */
+    public function an_authenticated_user_and_with_role_admin_can_create_categories(User $user): void
+    {
+        $this->actingAs($user);
+
+        $response = $this->post(route("categories.save"), [
+            "name" => "Camisas",
+            "icon" => '<i class="fas fa-car"></i>'
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("categories.index"));
+
         $this->assertDatabaseHas("categories", [
-            "name" => "Camisas", "icon" => e('<i class="fas fa-car"></i>')
+            "name" => "Camisas"
         ]);
     }
 }
